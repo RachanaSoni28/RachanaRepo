fc98d5c031032d82d58ac31fb0f0e05a441a592e
rachansoni28 <rachanasoni28@gmail.com>
2024-12-15 00:12:28 +0530

    Commit in main

--- src/main/Test2.java
+++ src/main/Test2.java
@@ -2,10 +2,7 @@ package src.main;
 
 public class Test2 {
     public static void main(String[] args) {
-<<<<<<< HEAD
         System.out.println("Hello Rachana");
-=======
         System.out.println("Hello Rachana Soni");
->>>>>>> c13cbac52be41385e68d1483e56ca4521fe55e7c
     }
 }
