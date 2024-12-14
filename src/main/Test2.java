4b5e8d78e562439fe11184329e28df19c3f48816
RachanaSoni28 <rachanasoni28@gmail.com>
2024-12-15 00:18:29 +0530

    Pull request from main to master (#6)
    * Test class changes (#1)
    
    * Test class
    
    * new test class
    
    * Commit in main
    
    * Pull request from feature to main (#5)
    
    * Test class
    
    * new test class
    
    * Adding changes by lastname
    
    * Change in feature branch
    
    * Added two comment lines
    
    * Last change
    
    * Coomit in feature

--- src/main/Test2.java
+++ src/main/Test2.java
@@ -2,6 +2,9 @@ package src.main;
 
 public class Test2 {
     public static void main(String[] args) {
+        System.out.println("Hello Rachana");
         System.out.println("Hello Rachana Soni");
+        System.out.println("Hello Rachana");
+        System.out.println("Hello Soni");
     }
 }
