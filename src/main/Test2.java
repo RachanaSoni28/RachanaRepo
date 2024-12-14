28528fe83c14974a9bef57af0159e42f92e7e412
rachansoni28 <rachanasoni28@gmail.com>
2024-12-14 16:22:16 +0530

    Adding changes by lastname

--- src/main/Test2.java
+++ src/main/Test2.java
@@ -2,6 +2,6 @@ package src.main;
 
 public class Test2 {
     public static void main(String[] args) {
-        System.out.println("Hello Rachana");
+        System.out.println("Hello Rachana Soni");
     }
 }
